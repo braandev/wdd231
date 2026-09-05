bac98d8af637a5867165f7ba47c1575a6bcf4c73
braandev <brandondev.contacto@gmail.com>
2026-09-05 08:57:49 -0300

    upload of first commi, first week

---
+++ scripts/date.js
@@ -0,0 +1,3 @@
+document.getElementById("copyright").innerHTML = `&copy; ${new Date().getFullYear()} - Brandon Cazorla - Argentina`;
+
+document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
